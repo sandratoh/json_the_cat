4e404f63a32b697009d9e9920ee5c677150d9f53
Sandra To <sandra.to.e@gmail.com>
2021-02-11 22:49:33 +0000

    node app display message if breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,17 @@
 const request = require('request');
-let args = process.argv.slice(2);
 
+let args = process.argv.slice(2);
 let query = args.toString();
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, (error, response, body) => {
-  
-  // deserialization: parse string to object
-  const data = JSON.parse(body);
-  // body becomes string in an array of one object
-  console.log(data[0].description);
+  if (error || response.statusCode !== 200) {
+    console.log('Hmmm, there seems to be some problem with the webpage. Check the URL and try again! (=•ｪ•=?');
+  }
+  if (body === '[]') {
+    console.log('Oh no! We can\'t find the breed you are looking. Check your spelling and try again! ฅ^•ﻌ•^ฅ');
+  } else {
+    // deserialization: parse string to object
+    const data = JSON.parse(body);
+    console.log(data[0].description, 'ฅ^◕ﻌ◕^ฅ ♥');
+  }
 });

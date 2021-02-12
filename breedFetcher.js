c9aa00d3a9ede49a685ede109193f93eb51eb9d3
Sandra To <sandra.to.e@gmail.com>
2021-02-12 00:31:41 +0000

    Removed linting error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,5 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-
 const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
     if (err || response.statusCode !== 200) {
